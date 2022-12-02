ca0b19faa57b5e0b123f03597d821e593be50cd9
JeanJacquesNS <jeanjacquesns3@gmail.com>
2022-12-02 13:04:04 +0200

    removido o erro que impedia a inicializacao

---
+++ src/test/java/com/jeanjacquesns/springapi/springapi/SpringapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanjacquesns.springapi.springapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
