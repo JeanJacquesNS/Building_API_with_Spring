dc2cd22cc7cd6b3ccf36cd00a054c9946e73700a
JeanJacquesNS <jeanjacquesns3@gmail.com>
2022-12-02 10:59:13 +0200

    First commit

---
+++ src/test/java/com/jeanjacquesns/spiringapi/SpiringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanjacquesns.spiringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
